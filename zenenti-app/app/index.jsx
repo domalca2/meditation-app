2bf5c867d4c7a37d3503a371627b66c033f88aa9
Gaston82 <cabezaartaza@hotmail.com>
2024-10-03 06:26:30 +0200

    Apply fonts to the app

--- zenenti-app/app/index.jsx
+++ zenenti-app/app/index.jsx
@@ -1,13 +1,21 @@
 import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
+import { SafeAreaView, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View className="mt-10">
-      <Text className="text-3xl">
-        Open up App.js to start working on your app test!
-      </Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+      <View className="mt-6 mx-2.5">
+        <Text className="text-5xl font-alegra-bold">
+          Open up App.js to start working on your app test!
+        </Text>
+        <Text className="text-3xl font-alegra-regular">
+          Open up App.js to start working on your app test!
+        </Text>
+        <Text className="text-1xl font-alegra-medium">
+          Open up App.js to start working on your app test!
+        </Text>
+        <StatusBar style="auto" />
+      </View>
+    </SafeAreaView>
   );
 }
