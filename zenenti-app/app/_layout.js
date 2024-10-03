01cd708a68bfd19c29d8600965bc75d927849cd3
Gaston82 <cabezaartaza@hotmail.com>
2024-10-03 06:19:59 +0200

    Add fonts to _layout file

--- zenenti-app/app/_layout.js
+++ zenenti-app/app/_layout.js
@@ -1,6 +1,29 @@
-import { Slot } from "expo-router";
+import { Slot, SplashScreen } from "expo-router";
+import { useFonts } from "expo-font";
 import "./global.css";
+import { useEffect } from "react";
+
+SplashScreen.preventAutoHideAsync();
 const RootLayout = () => {
+  const [fontsLoaded, error] = useFonts({
+    "AlegreyaSans-Medium": require("../assets/fonts/AlegreyaSans-Medium.ttf"),
+    "AlegreyaSans-Regular": require("../assets/fonts/AlegreyaSans-Regular.ttf"),
+    "AlegreyaSans-Bold": require("../assets/fonts/AlegreyaSans-Bold.ttf"),
+  });
+
+  useEffect(() => {
+    if (error) {
+      throw error;
+    }
+
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, error]);
+
+  if (!fontsLoaded && !error) {
+    return null;
+  }
   return <Slot />;
 };
 
