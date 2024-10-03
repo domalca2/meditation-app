72906757655b3e893a76f04ad038ae2ad1bda557
Gaston82 <cabezaartaza@hotmail.com>
2024-10-03 06:42:41 +0200

    Apply colors

--- zenenti-app/app/index.jsx
+++ zenenti-app/app/index.jsx
@@ -4,14 +4,14 @@ import { SafeAreaView, Text, View } from "react-native";
 export default function App() {
   return (
     <SafeAreaView>
-      <View className="mt-6 mx-2.5">
-        <Text className="text-5xl font-alegra-bold">
+      <View className="mt-6 mx-2.5 bg-primary p-10">
+        <Text className="text-5xl font-alegra-bold text-secondary">
           Open up App.js to start working on your app test!
         </Text>
         <Text className="text-3xl font-alegra-regular">
           Open up App.js to start working on your app test!
         </Text>
-        <Text className="text-1xl font-alegra-medium">
+        <Text className="text-1xl font-alegra-medium text-bgMain">
           Open up App.js to start working on your app test!
         </Text>
         <StatusBar style="auto" />
