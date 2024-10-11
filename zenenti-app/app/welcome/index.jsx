5f44f96eecd1dc7fea42cb59d6c946e1ae1d28f4
Gaston82 <cabezaartaza@hotmail.com>
2024-10-11 09:57:05 +0200

    add welcome screen page #19

---
+++ zenenti-app/app/welcome/index.jsx
@@ -0,0 +1,32 @@
+import { View, Text, SafeAreaView, Image, StatusBar } from "react-native";
+import React from "react";
+import Button from "../../components/Button";
+
+const WelcomeScreen = () => {
+  const brand = require("../../assets/images/brand.png");
+  const logo = require("../../assets/images/logo-zenenti.png");
+  return (
+    <SafeAreaView className="flex-1">
+      <View className=" bg-primary flex-1 py-14">
+        <View className="px-14">
+          <Image source={logo} />
+        </View>
+        <View className="px-12">
+          <Image source={brand} />
+        </View>
+        <View className="px-7 my-11 ">
+          <Text className="text-2xl text-center font-alegra-medium text-white">
+            Respira profundamente
+          </Text>
+          <Text className="text-2xl text-center font-alegra-medium text-white">
+            Empieza un nuevo camino
+          </Text>
+        </View>
+        <Button text={"comienza"} />
+        <StatusBar style="auto" />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default WelcomeScreen;
