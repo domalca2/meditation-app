420872a94bd5de0ff9f064f69bf83426163e9fbd
Gaston82 <cabezaartaza@hotmail.com>
2024-10-11 09:56:23 +0200

    add redirect to index.jsx #19

--- zenenti-app/app/index.jsx
+++ zenenti-app/app/index.jsx
@@ -1,30 +1,5 @@
-import { StatusBar } from "expo-status-bar";
-import { Image, SafeAreaView, Text, View } from "react-native";
-import Button from "../components/Button";
-const brand = require("../assets/images/brand.png");
-const logo = require("../assets/images/logo-zenenti.png");
+import { Redirect } from "expo-router";
 
 export default function App() {
-  return (
-    <SafeAreaView className="flex-1">
-      <View className=" bg-primary flex-1 py-14">
-        <View className="px-14">
-          <Image source={logo} />
-        </View>
-        <View className="px-12">
-          <Image source={brand} />
-        </View>
-        <View className="px-7 my-11 ">
-          <Text className="text-2xl text-center font-alegra-medium text-white">
-            Respira profundamente
-          </Text>
-          <Text className="text-2xl text-center font-alegra-medium text-white">
-            Empieza un nuevo camino
-          </Text>
-        </View>
-        <Button text={"comienza"} />
-        <StatusBar style="auto" />
-      </View>
-    </SafeAreaView>
-  );
+  return <Redirect href={"/welcome"} />;
 }
