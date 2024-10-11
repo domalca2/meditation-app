5ed154f715aa6d159003d36965e7c9a1d5360f76
Gaston82 <cabezaartaza@hotmail.com>
2024-10-11 10:56:27 +0200

    add onPress to Button component #19

--- zenenti-app/components/Button.jsx
+++ zenenti-app/components/Button.jsx
@@ -1,9 +1,12 @@
 import { Text, Pressable } from "react-native";
 import React from "react";
 
-const Button = ({ text }) => {
+const Button = ({ text, onPress }) => {
   return (
-    <Pressable className="bg-secondary rounded-2xl py-4  mx-4 mb-14">
+    <Pressable
+      onPress={onPress}
+      className="bg-secondary rounded-2xl py-4  mx-4 mb-14"
+    >
       <Text className="text-center text-white font-alegra-medium text-4xl uppercase">
         {text}
       </Text>
