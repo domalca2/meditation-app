fadd7866126972ab6d159ce839cf0ad717c18ece
Nathan Seymour <nathan@seymour.global>
2024-10-15 17:27:20 -0500

    add enable property to Button.jsx #33

--- zenenti-app/components/Button.jsx
+++ zenenti-app/components/Button.jsx
@@ -1,11 +1,11 @@
 import { Text, Pressable } from "react-native";
 import React from "react";
 
-const Button = ({ text, onPress }) => {
+const Button = ({ text, onPress, enabled = true }) => {
   return (
     <Pressable
-      onPress={onPress}
-      className="bg-secondary rounded-2xl py-4  mx-4 mb-14"
+      onPress={enabled ? onPress : undefined}
+      className={`${enabled ? "bg-secondary" : "bg-disabled"} rounded-2xl py-4  mx-4 mb-14`}
     >
       <Text className="text-center text-white font-alegra-medium text-4xl uppercase">
         {text}
