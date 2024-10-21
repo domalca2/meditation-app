2043bc28a44a571fae46d1edfab1b8403998765f
Gaston82 <cabezaartaza@hotmail.com>
2024-10-21 19:18:52 +0200

    add welcome3 stack #35

--- zenenti-app/app/(stack)/_layout.jsx
+++ zenenti-app/app/(stack)/_layout.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 import { Stack } from "expo-router";
+import { UserProvider } from "../../contexts/userContext";
 
 const StackLayout = () => {
   return (
-    <Stack>
-      <Stack.Screen name="splash/index" options={{ headerShown: false }} />
-      <Stack.Screen name="welcome/index" options={{ title: "Welcome" }} />
-      <Stack.Screen name="greeting/index" options={{ title: "Greeting" }} />
-      <Stack.Screen name="register/index" options={{ title: "Register" }} />
-    </Stack>
+    <UserProvider>
+      <Stack>
+        <Stack.Screen name="splash/index" options={{ headerShown: false }} />
+        <Stack.Screen name="welcome/index" options={{ title: "Welcome" }} />
+        <Stack.Screen name="greeting/index" options={{ title: "Greeting" }} />
+        <Stack.Screen name="register/index" options={{ title: "Register" }} />
+        <Stack.Screen
+          name="welcome3/index"
+          options={{ title: "Welcome user" }}
+        />
+      </Stack>
+    </UserProvider>
   );
 };
 
