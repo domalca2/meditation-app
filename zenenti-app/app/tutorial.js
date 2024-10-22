0d9189d34dcce09da259efa3af43448ece59d22c
Nathan Seymour <nathan@seymour.global>
2024-10-22 09:27:46 -0500

    add subtitles for first video #44

--- zenenti-app/app/tutorial.js
+++ zenenti-app/app/tutorial.js
@@ -1,34 +1,73 @@
-const tutorial = {
-  introduction: {
+const tutorial = [
+  {
+    name: "introduction",
     title: "Inicio",
-    steps: [
+    audio: require("../assets/audio/tutorial-1.m4a"),
+    subtitles: [
       {
-        message: "Felicidades por iniciar tu camino de meditación.",
-        audio: "",
+        start: 0,
+        message: "Hola. Te doy la bienvenida a Zenenti. Y te felicito por haber dado el primer paso en tu camino de meditación.",
       },
       {
-        message: "Aquí encontrarás herramientas y consejos. Con tu constancia, tu semilla florecerá.",
-        audio: "",
+        start: 14,
+        message: "¡Enhorabuena! Ya sabes lo que dicen: lo más complicado es empezar. Y tú ya lo has hecho.",
+      },
+      {
+        start: 22,
+        message: "Si has llegado hasta aquí tal vez hayas oído hablar alguna vez de la meditación y todas las cosas buenas que puede proporcionarte. Quizá necesites calmar tu mente si está demasiado agitada. O buscas dormir mejor.",
+      },
+      {
+        start: 41,
+        message: "Sea cual sea tu caso, aquí encontrarás herramientas y consejos para que esa semilla que has plantado hoy crezca y florezca.",
+      },
+      {
+        start: 53,
+        message: "Conforme avances en tu práctica, tu semilla irá creciendo y podrás verla convertida en una preciosa flor si mantienes la constancia y pasas por aquí a menudo. Podrás ir comprobando tu propio progreso y elegir qué necesitas cada día para sentirte mejor.",
+      },
+      {
+        start: 78,
+        message: "Meditaciones, ejercicios, juegos de atención plena, desafíos… Poco a poco te iremos contando más de todo lo que podrás encontrar en Zenenti.",
+      },
+      {
+        start: 91,
+        message: "Ahora, para acompañarte en este apasionante viaje, voy a darte algunas pistas interesantes que te ayuden a comenzar tu práctica de meditación.",
+      },
+      {
+        start: 104,
+        message: "¡Vamos allá!",
       },
     ],
   },
-  space: {
+  {
+    name: "space",
     title: "Espacio",
-    steps: [
-      {
-        message: "Para empezar, elige un espacio tranquilo para meditar.",
-        audio: "",
-      },
-      {
-        message: "Un rincón especial donde puedas desconectar y sentir calma.",
-        audio: "",
-      },
-      {
-        message: "Puedes personalizar ese espacio para que te motive a regresar.",
-        audio: "",
-      },
-    ],
+    audio: require("../assets/audio/tutorial-2.m4a"),
+    subtitles: [],
   },
-};
+  {
+    name: "posture",
+    title: "Postura",
+    audio: require("../assets/audio/tutorial-3.m4a"),
+    subtitles: [],
+  },
+  {
+    name: "calm",
+    title: "Calma",
+    audio: require("../assets/audio/tutorial-4.m4a"),
+    subtitles: [],
+  },
+  {
+    name: "breathe",
+    title: "Respira",
+    audio: require("../assets/audio/tutorial-5.m4a"),
+    subtitles: [],
+  },
+  {
+    name: "observe",
+    title: "Observa",
+    audio: require("../assets/audio/tutorial-6.m4a"),
+    subtitles: [],
+  },
+];
 
 export default tutorial;
