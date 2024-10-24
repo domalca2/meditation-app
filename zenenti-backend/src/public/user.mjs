ebfacff2d9382ca9f1ca2fb097e71a50cd67844b
Nathan Seymour <nathan@seymour.global>
2024-10-24 10:39:47 -0500

    add test for database integration with prisma #62

--- zenenti-backend/src/public/user.mjs
+++ zenenti-backend/src/public/user.mjs
@@ -1,16 +1,25 @@
 import { Router } from "express";
+import db from "../db/db.mjs";
 import validate from "../middleware/validate.mjs";
 
 const router = Router();
 
 const registerSchema = {
-  email: {
-    isEmail: true,
-  },
+  user_name:  { isString: true },
+  email:      { isEmail: true },
+  age:        { isInt: true },
+  gender:     { isString: true },
+  education:  { isString: true },
+  profession: { isString: true },
+  country:    { isString: true }
 };
 
-router.post("/register", validate(registerSchema), (req, res) => {
-  res.sendStatus(200);
+router.post("/register", validate(registerSchema), async (req, res) => {
+  const user = await db.user.create({
+    data: req.body
+  });
+
+  res.json(user);
 });
 
 export default router;
