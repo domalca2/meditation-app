c920f8b588e57b8a8259d4bb5172a3945d4ef721
Nathan Seymour <nathan@seymour.global>
2024-10-30 13:17:52 -0500

    add home page and show username #79 #80

---
+++ zenenti-app/app/(stack)/home/index.jsx
@@ -0,0 +1,27 @@
+import { SafeAreaView, Text, View } from "react-native";
+import { mockQuery } from "../../../mock/mock";
+import { useQuery } from "@tanstack/react-query";
+
+const Home = () => {
+  const usernameQuery = useQuery({
+    queryKey: ["user/name"],
+    queryFn: mockQuery("user/name"),
+  });
+
+  return (
+    <SafeAreaView className="flex-1 px-5 py-16">
+      <View className="flex flex-col">
+        {usernameQuery.isSuccess && (
+          <View>
+            <Text className="w-full font-alegra-medium text-4xl">
+              <Text className="font-bold">Hola, </Text>
+              <Text>{usernameQuery.data}</Text>
+            </Text>
+          </View>
+        )}
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default Home;
