2657bd17d846a9cf9d424edc55e4a1d8b3b6c12c
Nathan Seymour <nathan@seymour.global>
2024-11-03 05:36:03 -0800

    update explore screen background color #88

--- zenenti-app/app/main/explore/index.jsx
+++ zenenti-app/app/main/explore/index.jsx
@@ -1,9 +1,9 @@
-import { View, Text, SafeAreaView } from "react-native";
+import { Text, SafeAreaView } from "react-native";
 import CategorySelect from "../../../components/category/CategorySelect";
 
 const Explore = () => {
   return (
-    <SafeAreaView className="flex-1 flex-col py-16 px-5">
+    <SafeAreaView className="flex-1 flex-col py-16 px-5 bg-bgMain">
       <Text className="w-full text-center font-alegra-bold text-2xl mb-6">
         Explorar
       </Text>
