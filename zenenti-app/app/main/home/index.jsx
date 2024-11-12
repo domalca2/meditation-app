3a6002e2295c81776419054fa304a36dfe9e32df
Nathan Seymour <nathan@seymour.global>
2024-11-12 09:05:29 -0600

    add circle to home screen #85

--- zenenti-app/app/main/home/index.jsx
+++ zenenti-app/app/main/home/index.jsx
@@ -3,14 +3,14 @@ import { mockQuery } from "../../../mock/mock";
 import { useQuery } from "@tanstack/react-query";
 import { useRouter } from "expo-router";
 
-import circle from "../../../assets/images/patata.jpg";
+import circle from "../../../assets/images/patata.png";
 import flor from "../../../assets/images/flor-sugerencias.png";
 import PracticeCard from "../../../components/PracticeCard";
 
 const Home = () => {
   const router = useRouter();
 
-  const usernameQuery = useQuery({
+  const username = useQuery({
     queryFn: mockQuery("user/name"),
     queryKey: ["user", "name"],
   });
@@ -20,6 +20,11 @@ const Home = () => {
     queryKey: ["practice", "practices"],
   });
 
+  const quotes = useQuery({
+    queryFn: mockQuery("quotes"),
+    queryKey: ["quotes"],
+  });
+
   const startPractice = (id) => {
     router.push(`/practice/${id}`);
   };
@@ -27,40 +32,48 @@ const Home = () => {
   return (
     <SafeAreaView className="flex-1 px-4 py-14 bg-bgMain">
       <View className="flex flex-col">
-        {usernameQuery.isSuccess && (
+        {username.isSuccess && (
           <View>
             <Text className="w-full font-alegra-medium text-4xl">
               <Text className="font-bold">Hola, </Text>
-              <Text>{usernameQuery.data}</Text>
+              <Text>{username.data}</Text>
             </Text>
           </View>
         )}
-        <View className="flex flex-row mt-6 justify-evenly">
-          <Image source={circle} className="w-14 h-16" />
-          <View className="max-sm:m-3 justify-center">
-            <Text>"Dar no nos empobrece, ni retener nos enriquece"B.K.S</Text>
+        <View className="flex flex-row py-10">
+          <View className="flex items-center justify-center bg-quaternary rounded-full p-2">
+            <View className="bg-bgMain rounded-full p-5">
+              <Image className="w-16 h-16" source={circle} />
+            </View>
+          </View>
+          <View className="flex-grow justify-center items-center">
+            {quotes.isSuccess && (
+              <Text className="font-alegra-medium color-[#626161] text-l w-3/4">
+                {quotes.data[Math.floor(Math.random() * quotes.data.length)]}
+              </Text>
+            )}
           </View>
         </View>
-        <Text className="font-alegra-medium text-2xl mt-[25]">
-          ¿Qué necesitas hoy?
-        </Text>
+        <Text className="font-alegra-medium text-2xl">¿Qué necesitas hoy?</Text>
         <View className="flex bg-primary mt-5 mb-5 flex-row w-[150] p-2 justify-evenly rounded-full">
           <Image className="w-5 h-5" source={flor} />
           <Text className="text-white">Sugerencias</Text>
         </View>
-        <FlatList
-          contentContainerStyle={{ gap: 15 }}
-          data={practices.data}
-          keyExtractor={(practice) => practice.durationMillis}
-          renderItem={({ item }) => (
-            <PracticeCard
-              practice={item}
-              onPress={() => {
-                startPractice(item.id);
-              }}
-            />
-          )}
-        />
+        {practices.isSuccess && (
+          <FlatList
+            contentContainerStyle={{ gap: 15 }}
+            data={practices.data}
+            keyExtractor={(practice) => practice.durationMillis}
+            renderItem={({ item }) => (
+              <PracticeCard
+                practice={item}
+                onPress={() => {
+                  startPractice(item.id);
+                }}
+              />
+            )}
+          />
+        )}
       </View>
     </SafeAreaView>
   );
