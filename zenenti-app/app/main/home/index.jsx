8972efc95b56a899b1f77c036bcdc49d160c5f70
Gaston82 <cabezaartaza@hotmail.com>
2024-11-12 11:12:32 +0100

    add FlatList to Home #106

--- zenenti-app/app/main/home/index.jsx
+++ zenenti-app/app/main/home/index.jsx
@@ -1,15 +1,24 @@
-import { SafeAreaView, Text, View } from "react-native";
+import React from "react";
+import { Image, SafeAreaView, Text, View, FlatList } from "react-native";
 import { mockQuery } from "../../../mock/mock";
 import { useQuery } from "@tanstack/react-query";
+
+import circle from "../../../assets/images/patata.jpg";
+import flor from "../../../assets/images/flor-sugerencias.png";
+import PracticeCard from "../../../components/PracticeCard";
 
 const Home = () => {
   const usernameQuery = useQuery({
     queryKey: ["user/name"],
     queryFn: mockQuery("user/name"),
   });
+  const practices = useQuery({
+    queryFn: mockQuery("practice/practices"),
+    queryKey: ["practice", "practices"],
+  });
 
   return (
-    <SafeAreaView className="flex-1 px-5 py-16 bg-bgMain">
+    <SafeAreaView className="flex-1 px-4 py-14 bg-bgMain">
       <View className="flex flex-col">
         {usernameQuery.isSuccess && (
           <View>
@@ -19,6 +28,24 @@ const Home = () => {
             </Text>
           </View>
         )}
+        <View className="flex flex-row mt-6 justify-evenly">
+          <Image source={circle} className="w-14 h-16" />
+          <View className="max-sm:m-3 justify-center">
+            <Text>"Dar no nos empobrece, ni retener nos enriquece"B.K.S</Text>
+          </View>
+        </View>
+        <Text className="font-alegra-medium text-2xl mt-[25]">
+          ¿Qué necesitas hoy?
+        </Text>
+        <View className="bg-primary mt-5 mb-5 flex-row w-[150] p-2 justify-evenly rounded-[100]">
+          <Image source={flor} />
+          <Text className="text-white">Sugerencias</Text>
+        </View>
+        <FlatList
+          data={practices.data}
+          keyExtractor={(practice) => practice.durationMillis}
+          renderItem={({ item }) => <PracticeCard practice={item} />}
+        />
       </View>
     </SafeAreaView>
   );
