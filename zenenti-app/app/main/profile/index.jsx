a2b256effc450ff749dc3b00077e253025e3062e
Pablo <pablobezard@gmail.com>
2024-12-05 10:54:52 -0300

    styles in nativewind and porfile-picture changes

--- zenenti-app/app/main/profile/index.jsx
+++ zenenti-app/app/main/profile/index.jsx
@@ -2,6 +2,7 @@ import { SafeAreaView, Text, View } from "react-native";
 import { useQuery } from "@tanstack/react-query";
 import { mockQuery } from "../../../mock/mock";
 import ProfileBubble from "../../../components/ProfileBubble";
+import Streak from "./Streak"; 
 
 const Profile = () => {
   const username = useQuery({
@@ -11,6 +12,7 @@ const Profile = () => {
 
   return (
     <SafeAreaView className="flex-1 px-4 py-14 bg-bgMain">
+      
       <View className="flex flex-col">
         {username.isSuccess && (
           <View>
@@ -21,14 +23,20 @@ const Profile = () => {
           </View>
         )}
       </View>
+     
       <View className="flex flex-row justify-end px-10">
         <ProfileBubble />
       </View>
+     
       <View>
         <Text className="font-alegra-bold text-2xl mb-2">Semilla</Text>
         <Text className="font-alegra-medium">
           Potencial, posibilidades y nuevos comienzos.
         </Text>
+      
+        <View style={{ marginTop: 100, paddingHorizontal: 10 }}>
+          <Streak />
+        </View>
       </View>
     </SafeAreaView>
   );
